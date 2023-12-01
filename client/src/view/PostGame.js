7ae262ecbd8159199441ad05b2b46d14edf81bcf
oro kang <dkang8@ncsu.edu>
2023-11-30 21:10:26 -0500

    before implementing file upload

--- client/src/view/PostGame.js
+++ client/src/view/PostGame.js
@@ -1,7 +1,10 @@
 import './PostGame.css'
 import Footer from '../components/Footer';
 import dayjs from 'dayjs';
-import {useEffect, useState} from 'react';
+import {useEffect, useState, useRef} from 'react';
+import Box from '@mui/material/Box';
+
+
 import {
     OutlinedInput,
     InputLabel,
@@ -27,10 +30,45 @@ const genres = [
 
 export default function PostGame(){
 
+    window.addEventListener("scroll", setFixed);
+    const navRef = useRef(null);
+    //setting nav's display style 
+    const [isFix, setFix] = useState(false);
+    function setFixed(){
+        //setWindowYSize(window.scrollY );
+        const y = window.scrollY;
+        //console.log(y);
+        if(y >= 82.66667){
+            
+            //document.getElementById("root").addClassList();
+            document.getElementById("root").style.paddingTop = navRef.offsetHeight;
+            setFix(true);
+
+        }else{
+            setFix(false);
+        }
+        
+    }
+
+
+  
+
+    
+
+
+
     //for date picker in <devInfoContainer>
     const [dateValue, setDateValue] = useState(dayjs('2022-04-17'));
 
+    //setting genres from <devInfoContainer>
     const [selectedGenres, setSelectedGenres] = useState([]);
+
+
+    const [sys, setSys] = useState('');
+
+    const handleChange = (event) => {
+        setSys(event.target.value);
+    };
 
     useEffect(()=>{
         console.log("selectedGenres", selectedGenres);
@@ -38,87 +76,132 @@ export default function PostGame(){
 
     return(
         <div className='wrapper'>
-        <section className='postGameSection'>
-            <nav>
-                <div>
-                <div className='title nav postGame'>Jörmungandr</div>
-                <div className='listContainer'>
-                    <ul>
-                        <li>Devleoper's information</li>
-                        <li>System requirement</li>
-                        <li>pictures</li>
-                        <li>Agreement</li>
-                    </ul>
-                </div>
-                </div>
-            </nav>
-        </section>
-
-        <section className='devInfoSection'>
-            <div className='devInfoContainer'>
-                <div>
-                    <label>
-                        Title
-                        <input type="text"></input>
-                    </label>
-                    <label>
-                        Price
-                        <input type="text"></input>
-                    </label>
-                </div>
-                <label>
-                    Description
-                    <textarea></textarea>
-                </label>
-
-                <div>
-                    <FormControl sx={{ m: 1, width: 500 }}>
-                    <InputLabel>Multiple Select</InputLabel>
-                    <Select
-                    multiple
-                    value={selectedGenres}
-                    onChange={(e) => setSelectedGenres(e.target.value)}
-                    input={<OutlinedInput label="Multiple Select" />}
-                    renderValue={(selected) => (
-                    <Stack gap={1} direction="row" flexWrap="wrap">
-                        {selected.map((value) => (
-                        <Chip
-                            key={value}
-                            label={value}
-                            onDelete={() =>
-                            setSelectedGenres(
-                                selectedGenres.filter((item) => item !== value)
-                            )
-                            }
-                            deleteIcon={
-                                <CancelIcon
-                                    onMouseDown={(event) => event.stopPropagation()}
-                                />
-                                }
-                            />
-                            ))}
-                            </Stack>
-                    )}
-                    >
-                    {genres.map((name) => (
-                    <MenuItem key={name} value={name}>
-                        {name}
-                    </MenuItem>
-                    ))}
-                    </Select>
-                    </FormControl>
-                    </div>
-
+            <header className={isFix ?'menuNavFixed postGameSection' : `menuNav postGameSection`} ref={navRef} >
+                <nav>
                     <div>
-                        <button>next</button>
+                    <div className='title nav postGame'>Jörmungandr</div>
+                    <div className='listContainer'>
+                        <ul>
+                            <li>Devleoper's information</li>
+                            <li>System requirement</li>
+                            <li>pictures</li>
+                            <li>Agreement</li>
+                        </ul>
                     </div>
+                    </div>
+                </nav>
+            </header>
+
+            <section className='devInfoSection'>
+                <div className='devInfoContainer'>
+                    <div>
+                        <label>
+                            <text>Title</text>
+                            <input type="text"></input>
+                        </label>
+                        <label>
+                            <text>Price</text>
+                            <input type="text"></input>
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            <text>Description</text>
+                            <textarea></textarea>
+                        </label>
+                    </div>
+                    <div>
+                        <FormControl sx={{ m: 1, width: 500 }}>
+                        <InputLabel>Multiple Select</InputLabel>
+                        <Select
+                        multiple
+                        value={selectedGenres}
+                        onChange={(e) => setSelectedGenres(e.target.value)}
+                        input={<OutlinedInput label="Multiple Select" />}
+                        renderValue={(selected) => (
+                        <Stack gap={1} direction="row" flexWrap="wrap">
+                            {selected.map((value) => (
+                            <Chip
+                                key={value}
+                                label={value}
+                                onDelete={() =>
+                                setSelectedGenres(
+                                    selectedGenres.filter((item) => item !== value)
+                                )
+                                }
+                                deleteIcon={
+                                    <CancelIcon
+                                        onMouseDown={(event) => event.stopPropagation()}
+                                    />
+                                    }
+                                />
+                                ))}
+                                </Stack>
+                        )}
+                        >
+                        {genres.map((name) => (
+                        <MenuItem key={name} value={name}>
+                            {name}
+                        </MenuItem>
+                        ))}
+                        </Select>
+                        </FormControl>
+                        </div>
+
+                        <div>
+                            <button>next</button>
+                        </div>
+                </div>
+            </section>
+            <section className='sysReqSection'>
+            <div>
+                <label>
+                    <span>Graphic</span>
+                    <input type="text"></input>
+                </label>
+                <label>
+                    <span>Memory</span>
+                    <input type="text"></input>
+                </label>
+                <label>
+                    <span>Storage</span>
+                    <input type="text"></input>
+                </label>
+                <div>
+                <Box sx={{ minWidth: 120, maxWidth: 220 }}>
+                <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">platform</InputLabel>
+                    <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={sys}
+                    label="platform"
+                    onChange={handleChange}
+                    >
+                    <MenuItem value={10}>Windows</MenuItem>
+                    <MenuItem value={20}>Mac</MenuItem>
+                    <MenuItem value={30}>Linux</MenuItem>
+                    </Select>
+                </FormControl>
+                </Box>
+                </div>
+                <div>
+                    <button>next</button>
+                </div>
             </div>
-        </section>
-        <section className='sysReqSection'>
 
 
+            </section>
+            <section className='imageUploadSection'>
 
-        </section>
+            </section>
+            <section className='agreementSection'>
+                <div>
+                    <h2>Game Submission Agreement</h2>
+                    <p>Before submitting your game, please carefully read and agree to the following terms and conditions. By signing below, you acknowledge your understanding and acceptance of the terms outlined in this agreement.</p>
+                    
+                </div>
+            </section>
 
             <Footer/>
         </div>
